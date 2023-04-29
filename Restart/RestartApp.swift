0314196138bb03160b4d7fa5c12400e63cae9bb5
Filipe Fernandes <filipeferibeiro@outlook.com>
2023-04-28 22:22:20 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Filipe Fernandes on 28/04/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
