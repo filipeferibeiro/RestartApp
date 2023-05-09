86a2c6eae2991a807ddee6ecda876005f576bbfd
Filipe Fernandes <filipeferibeiro@outlook.com>
2023-05-09 11:47:26 -0300

    Project

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Filipe Fernandes on 28/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+        .animation(.easeOut(duration: 0.4), value: isOnboardingViewActive)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
